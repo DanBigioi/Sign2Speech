569cb4c6b32ddf5526195a7303619ca10245bbc2
Theo Morales <theo.morales.fr@gmail.com>
2022-02-03 19:35:58 +0000

    Add training script

---
+++ train.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 Théo Morales <theo.morales.fr@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Training script.
+"""
+
+from dataset import load_sign_alphabet
+from models.autoencoder import VAE
+
+import pytorch_lightning as pl
+
+
+def train():
+    train_loader, val_loader = load_sign_alphabet("DataSet.json", "dataset/spec/")
+    model = VAE()
+    trainer = pl.Trainer(gpus=1)
+    trainer.fit(model, train_loader, val_loader)
+
+
+if __name__ == "__main__":
+    # TODO: Use Hydra for experiment management and Weights&Biases for experiment logging
+    train()
