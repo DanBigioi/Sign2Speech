74c8d5696a86faffa46b701ecb0b4492a081a7ba
Theo Morales <theo.morales.fr@gmail.com>
2022-04-06 17:34:27 +0100

    Add plotting functions

--- test.py
+++ test.py
@@ -12,14 +12,17 @@ Testing script.
 
 from torchvision.io import read_image, ImageReadMode, write_jpeg
 
-from dataset import load_sign_alphabet
-from models.lstm import Sign2SpeechNet
 from models.autoencoder import AE_Deconv, AE
+from utils import plot_3D_hand, plot_pose
+from models.lstm import Sign2SpeechNet
+from dataset import load_sign_alphabet
 
+import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 import numpy as np
 import torchaudio
 import torch
+
 
 def test_gt(input_path: str):
     '''
@@ -45,10 +48,19 @@ def test_ae(model_path: str, input_path: str):
     ae = AE()
     model = ae.load_from_checkpoint(model_path)
     model.eval()
+
     sample_pose = torch.tensor(np.load(input_path).astype(np.float32)).unsqueeze(0)
-    specto = model(sample_pose).detach()[0] # Spectogram
+    # TODO: Remove this hack for real data (X and Y are swapped, and X is mirrored)
+    sample_pose = sample_pose.reshape((21, 3))
+    sample_pose[:, 1] = (1 - sample_pose[:, 1])
+    x = sample_pose[:, 1].clone()
+    sample_pose[:, 1] = sample_pose[:, 0].clone()
+    sample_pose[:, 0] = x
+    plot_3D_hand(sample_pose)
+
+    specto = model(sample_pose.flatten().unsqueeze(0)).detach()[0] # Spectogram
     print("Spectogram: ", specto.shape, specto.dtype)
-    write_jpeg((specto*255).type(dtype=torch.uint8), "test_specto.jpg")
+    write_jpeg((specto*255).type(dtype=torch.uint8), "spectogram.jpg")
 
     mel_inverter = torchaudio.transforms.InverseMelScale(sample_rate=sample_rate,
             n_stft=(n_fft//2)+1, n_mels=n_mels)
@@ -56,10 +68,11 @@ def test_ae(model_path: str, input_path: str):
     stft = mel_inverter(specto)
     print("STFT spectogram: ", stft.shape)
     pred_audio = griffin_limer(stft)
-    torchaudio.save("test.wav", pred_audio, sample_rate)
+    torchaudio.save("output.wav", pred_audio, sample_rate)
 
 
 if __name__ == "__main__":
     # TODO: Use Hydra for experiment management and Weights&Biases for experiment logging
     # test_gt("dataset/spec/h.png")
-    test_ae("ae_ckpt/Rcb8qHFjQd2jFBG4yDi9Bd/checkpoints/epoch=40-step=10331.ckpt", "dataset/train_poses/T/T  CY24.55P-0.08.npy")
+    test_ae("ae_ckpt/Rcb8qHFjQd2jFBG4yDi9Bd/checkpoints/epoch=40-step=10331.ckpt",
+            "/home/cactus/Downloads/W21749.npy")
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import json
 
@@ -39,3 +40,57 @@ def load_landmarks(filepath):
     landmarks = np.load(filepath)
     print("landmarks shape = " + str(landmarks.shape))
     return landmarks
+
+def plot_pose(ax, pose, plot_obj=True):
+    # Plot the hand first:
+    prev = pose[0, :]
+    for row in np.ndindex(pose.shape[0] - 8):
+        cur = pose[row, :]
+        if row[0] in [5, 9, 13, 17]:
+            prev = pose[0, :]
+        cur, prev = cur.flatten(), prev.flatten()
+        x, y, z = (
+            np.linspace(prev[0], cur[0], 100),
+            np.linspace(prev[1], cur[1], 100),
+            np.linspace(prev[2], cur[2], 100),
+        )
+        ax.plot(x, y, z, color="red")
+        ax.text(cur[0], cur[1], cur[2], f"{row[0]}", color="red")
+        prev = cur
+    if plot_obj:
+        # Plot the object bbox:
+        faces = [
+            [0, 1, 2, 3, 0],
+            [0, 1, 5, 4, 0],
+            [0, 3, 7, 4, 0],
+            [1, 5, 6, 2, 1],
+            [2, 3, 7, 6, 2],
+            [5, 6, 7, 4, 5]
+        ]
+        for face in faces:
+            prev = pose[21+face[0], :]
+            for idx in face:
+                row = 21 + idx
+                cur = pose[row, :]
+                cur, prev = cur.flatten(), prev.flatten()
+                x, y, z = (
+                    np.linspace(prev[0], cur[0], 100),
+                    np.linspace(prev[1], cur[1], 100),
+                    np.linspace(prev[2], cur[2], 100),
+                )
+                ax.plot(x, y, z, color="green")
+                ax.text(cur[0], cur[1], cur[2], f"{row-21}", color="green")
+                prev = cur
+
+#     scaling = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz'])
+#     ax.auto_scale_xyz(*[[np.min(scaling), np.max(scaling)]]*3)
+    world_limits = ax.get_w_lims()
+    ax.set_box_aspect((world_limits[1]-world_limits[0],world_limits[3]-world_limits[2],world_limits[5]-world_limits[4]))
+
+
+def plot_3D_hand(pose):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    plot_pose(ax, pose, plot_obj=False)
+    plt.show()
+
